9f0756d7bf6507806335c3674df6953a81342664
wangwei <182315544@qq.com>
2018-09-30 09:49:20 +0800

    sets config

--- eurekaclient/src/main/java/com/sznetweb/david/eureka/EurekaClientApplication.java
+++ eurekaclient/src/main/java/com/sznetweb/david/eureka/EurekaClientApplication.java
@@ -3,15 +3,27 @@ package com.sznetweb.david.eureka;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
-public class ConfigClientApplication {
+@RefreshScope
+public class EurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/")
+    public String home() {
+        return "hello world from port " + port;
     }
 
     @Value("${hello}")
